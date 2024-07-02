6f76cda7f461661d37b695be0cabc448c427b440
Suyel Bishnu <s2301242@BCVFVFLU1WG7Air.home>
2024-07-02 14:44:35 +0300

    Add more content & styling

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,9 +1,16 @@
-const Box = ({ name, title, location }) => {
+import React from "react";
+
+const Box = ({ id, name, title, age, location }) => {
   return (
-    <div className="box">
-      <p>{name}</p>
-      <p>{title}</p>
-      <p>{location}</p>
+    <div
+      className="card border border-danger mb-3"
+    >
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <h6 className="card-subtitle mb-2 text-muted">{title}</h6>
+        <p className="card-text">Age: {age}</p>
+        <p className="card-text">Location: {location}</p>
+      </div>
     </div>
   );
 };
