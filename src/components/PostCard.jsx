e60d8322559e874bd82c5ca75d97e01525144d58
Suyel Bishnu <s2301242@BCVFVFLU1WG7Air.home>
2024-07-02 14:45:43 +0300

    Add toggle publish or unpubish functionality

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,19 +1,36 @@
+import React from "react";
 
-export const PostCard = (props) => {
+const PostCard = ({
+  id,
+  title,
+  author,
+  content,
+  published,
+  date,
+  togglePublish,
+}) => {
+  const handleTogglePublish = () => {
+    togglePublish(id); // Call the togglePublish function passed from parent
+  };
+
   return (
-    <div>
-        <h3>{props.title}</h3>
-        <h4>{props.author}</h4>
-        <p>{props.content}</p>
-        <p>
-            {
-                props.published ? 'Published': 'Not Published'
-            }
-        </p>
-        <p>{props.date}</p>
+    <div className="col-md-4 mb-4">
+      <div className="card">
+        <div className="card-body">
+          <h3 className="card-title">{title}</h3>
+          <h4 className="card-subtitle mb-2 text-muted">{author}</h4>
+          <p className="card-text">{content}</p>
+          <p className="card-text">
+            {published ? "Published" : "Not Published"}
+          </p>
+          <p className="card-text">{date}</p>
+          <button className="btn btn-primary" onClick={handleTogglePublish}>
+            Change Status
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-
-
+export default PostCard;
