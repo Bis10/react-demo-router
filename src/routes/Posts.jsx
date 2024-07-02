41cb526c17d27a4f71457cd49bb3eb9568a2af8f
Suyel Bishnu <s2301242@BCVFVFLU1WG7Air.home>
2024-07-02 14:47:10 +0300

    Add new state to handle toggle

--- src/routes/Posts.jsx
+++ src/routes/Posts.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
-import { PostCard } from '../components/PostCard'
+import React, { useState } from "react";
+import PostCard from "../components/PostCard";
 
-const Posts = ({posts}) => {
+const Posts = ({ posts }) => {
+  // State to manage posts
+  const [allPosts, setAllPosts] = useState(posts);
+
+  // Function to toggle publish status
+  const handleTogglePublish = (postId) => {
+    const updatedPosts = allPosts.map((post) => {
+      if (post.id === postId) {
+        return { ...post, published: !post.published };
+      }
+      return post;
+    });
+    setAllPosts(updatedPosts); // Update the state with updated posts
+  };
+
   return (
-    <>
-    <h2>Posts will be here</h2>
-        {posts.map((post)=>(
-       <PostCard key={post.id} {...post}/>
+    <div className="container">
+      <h2 className="mt-4 mb-4">Posts will be here</h2>
+      <div className="row">
+        {allPosts.map((post) => (
+          <PostCard
+            key={post.id}
+            {...post}
+            togglePublish={() => handleTogglePublish(post.id)}
+          />
         ))}
-    </>
+      </div>
+    </div>
   );
 };
 
