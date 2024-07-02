a327344df41c5ed04dbd9929afcaa047291fea3c
Suyel Bishnu <s2301242@BCVFVFLU1WG7Air.home>
2024-07-02 14:46:02 +0300

    Add styling

--- src/routes/ErrorPage.jsx
+++ src/routes/ErrorPage.jsx
@@ -1,15 +1,24 @@
-import { useRouteError, useNavigate } from 'react-router-dom';
+import { useRouteError, useNavigate } from "react-router-dom";
 
 const ErrorPage = () => {
   const error = useRouteError();
   const navigate = useNavigate();
 
   return (
-    <div>
-      <h1>Ups, I did it again</h1>
-      <p>{error.statusText || error.message}</p>
-      <button onClick={() => navigate(-1)}>Go Back</button>
-    </div>
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+            <div className="card-body">
+              <h1 className="card-title">Ups, I did it again</h1>
+              <p className="card-text">{error.statusText || error.message}</p>
+              <button className="btn btn-primary" onClick={() => navigate(-1)}>
+                Go Back
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+   
   );
 };
 
